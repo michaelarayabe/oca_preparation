0d0c9d72e9d5a2833258b9003b6ef92f1a94c4c7
michaelarayabe <michaelseifuaraya@gmail.com>
2020-10-30 18:16:25 +0100

    Updated infos

---
+++ src/main/java/be/intecbrussel/java_basics/exercise/App.java
@@ -0,0 +1,10 @@
+package be.intecbrussel.java_basics.exercise;
+
+public class App {
+
+    public static void main(String[] args) {
+
+       for(int x = 3; x > -1; x--);
+        //System.out.println(x);
+    }
+}
