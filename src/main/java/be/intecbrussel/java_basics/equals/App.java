4c50aa1e7ca253b17d93b755bfde59aa334c7afc
michaelarayabe <michaelseifuaraya@gmail.com>
2020-11-02 10:36:36 +0100

    equals

--- src/main/java/be/intecbrussel/java_basics/equals/App.java
+++ src/main/java/be/intecbrussel/java_basics/equals/App.java
@@ -1,6 +1,7 @@
 package be.intecbrussel.java_basics.equals;
 /*
 In String == compares objects by reference.
+If we want to compare two objects (instances) excluding Strings the ‘equals' method returns false eventhough their value is the same, for it to be true we need to override 'equals' in the class. But when it comes to String, equals method only compare the literal value it holds and the == compares the reference value.
 
  */
 public class App {
