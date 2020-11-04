4828938cfa28104af51cffd65e6c2308b8d7bccf
michaelarayabe <michaelseifuaraya@gmail.com>
2020-11-04 11:04:17 +0100

    exceptions

---
+++ src/main/java/be/intecbrussel/java_basics/exceptions/App.java
@@ -0,0 +1,24 @@
+package be.intecbrussel.java_basics.exceptions;
+/*
+Checked exceptions
+
+Are exceptions that our programs recover from. They are enclosed by either: try catch or a method that provide throws clause.
+
+All exceptions are checked exceptions except: runtimeException and their subclasses and error.
+
+
+Unchecked exceptions
+Are the ones our program does not recover from. They are internal. Logical error, bugs, incorrect usage of api … They are indicated by RuntimeException and its subclasses.
+
+Errors
+
+Are also the one our program can not recover from, but they are external to the applicaton. Ex: StackOverflowErroe, OutOfMemoryError
+
+
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/be/intecbrussel/java_basics/object_reference_and_primitive/App.java
+++ src/main/java/be/intecbrussel/java_basics/object_reference_and_primitive/App.java
@@ -2,6 +2,7 @@ package be.intecbrussel.java_basics.object_reference_and_primitive;
 
 //passing primitive type into our method and object reference
 public class App {
+
     public static void main(String[] args) {
 
         App2 app2 = new App2(6);
@@ -12,10 +13,11 @@ public class App {
 
     }
 
+
     public static void sum(App2 app2){
         app2.value = app2.value + 10; // This method will affect our argument because our parameter calls value,
         //if it didn't and only manipulated app2 object, then it wont have any effect for our argument
-        //during invocation. Since we were just playing around a local variable.
+        //during invocation. Since we were just playing around with a local variable.
     }
 }
 
